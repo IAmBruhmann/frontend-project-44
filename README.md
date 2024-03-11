37aa6ef425f2b969362c76c7350e1e380ab59359
Nikita <vasickinfedor@gmail.com>
2024-03-11 10:57:21 +0300

    123321

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/IAmBruhmann/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/IAmBruhmann/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/6995fdbf197c3632a6a2/maintainability)](https://codeclimate.com/github/IAmBruhmann/frontend-project-44/maintainability)
-asciinema:
-    Brain-games:
-    Brain-calc:
+Описание проекта
+    "Игры разума" : это проект состоящий из 5 консольных игр, для тренировки вашего мозга. Каждая игра задает по 3 вопроса, на которые нужо дать правильные ответы. Если вы ответили на все вопросы, считается что вы прошли игру. В случае неправильного ответа игра завершится и вам будет предложено попробовать еще раз
+
+Установка
+
+    npm i
+
+    npm link
+Инструкция по запуску:
+
+апускает игру "Проверка на чётность"
+
+    brain-even
+
+запускает игру "Калькулятор"
+
+    brain-calc
+
+запускает игру "Наибольший общий делитель"
+
+    brain-gcd
+
+запускает игру "Арифметическая прогрессия"
+
+    brain-progression
+
+запускает игру "Простое ли число?"
+
+    brain-prime
---
+++ src/games/brain-progression.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node 
+ 
+import driver from '../index.js'; 
+import getRandomNumber from './random.js'; 
+ 
+const getProgression = () => { 
+  const length = 7; 
+  const hiddenIndex = Math.floor(Math.random() * length); 
+  const progression = []; 
+  let definableNumber; 
+  const start = getRandomNumber(0, 10); 
+  const diff = getRandomNumber(0, 10) + 1; 
+  for (let i = 1; i <= length; i += 1) { 
+    if (i === hiddenIndex + 1) { 
+      progression.push('..'); 
+      definableNumber = start + (i - 1) * diff; 
+    } else { 
+      const number = start + (i - 1) * diff; 
+      progression.push(number); 
+    } 
+  } 
+  const progression2 = progression.join(' '); 
+  return { progression2, definableNumber }; 
+}; 
+const question = 'What number is missing in the progression?'; 
+const playGame = () => { 
+  const prog = getProgression(0, 10); 
+  const progression = prog.progression2; 
+  const questionTwo = `${progression}`; 
+  const rightAnswer = prog.definableNumber.toString(); 
+  return { rightAnswer, questionTwo }; 
+}; 
+const playProgGame = () => driver(playGame, question); 
+export default playProgGame;
